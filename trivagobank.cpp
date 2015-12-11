5b1999281d63692cd1d1f2c853e721ee6eb3cb7a
natsud <kofi.offei.ko@gmail.com>
2015-12-11 21:42:35 +0100

    Create trivagobank.cpp

---
+++ trivagobank.cpp
@@ -0,0 +1,11 @@
+#include "TrivagoBank.h"
+
+TrivagoBank::TrivagoBank(const std::string& bankname, const std::string& bic, const std::string& countryAbbrv) : Bank(bankname, bic, countryAbbrv)
+{
+
+}
+
+TrivagoBank::~TrivagoBank()
+{
+	
+}
