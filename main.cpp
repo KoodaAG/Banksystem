07606cef8a694151d18d13cf2aee44ded912b049
natsud <kofi.offei.ko@gmail.com>
2015-12-11 21:39:19 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include "Bank.h"
+#include "BankAccount.h"
+#include "TrivagoBank.h"
+
+int main()
+{
+	TrivagoBank tvgBankDE("TrivagoBank", "TRVGDEHXXX", "DE");
+	std::getchar();
+	return 0;
+}
